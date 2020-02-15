1ae1e4c44726636bf455576c193768dd4e9f6048
chandra-joldao <cmanna7@gmail.com>
2020-02-15 23:48:21 +0530

    vnb

--- ajaxsubmit.php
+++ ajaxsubmit.php
@@ -3,10 +3,10 @@
     $email=$_POST['email'];
     $mobile=$_POST['mobile'];
     $message1=$_POST['message'];
-    $message = "<h1><b>Name:".$name."</b></h1>";
-    $message .= "<h1>Email:".$email."</h1>";
-    $message .= "<h1>Mobile:".$mobile."</h1>";
-    $message .= "<h1>Message:".$message1."</h1>";
+    $message = "<h1><b>Name: ".$name."</b></h1>";
+    $message .= "<h1>Email: ".$email."</h1>";
+    $message .= "<h1>Mobile: ".$mobile."</h1>";
+    $message .= "<h1>Message: ".$message1."</h1>";
     $to_email_address="cmanna7@gmail.com";
     //$to_email_address="info.nrsbs@gmail.com";
     $subject="Contact from ".$name;
