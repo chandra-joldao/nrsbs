d2d5a948ebec2f489df357175970e440eba550ab
chandra-joldao <cmanna7@gmail.com>
2020-02-15 23:45:15 +0530

    test

--- ajaxsubmit.php
+++ ajaxsubmit.php
@@ -8,9 +8,10 @@
     $message .= "<h1>Mobile:".$mobile."</h1>";
     $message .= "<h1>Message:".$message1."</h1>";
     $to_email_address="cmanna7@gmail.com";
+    //$to_email_address="info.nrsbs@gmail.com";
     $subject="Contact from ".$name;
-    $header = "From:abc@somedomain.com \r\n";
-    $header .= "Cc:afgh@somedomain.com \r\n";
+    $header = "From:".$email."\r\n";
+    $header .= "Cc:cmanna7@gmail.com \r\n";
     $header .= "MIME-Version: 1.0\r\n";
     $header .= "Content-type: text/html\r\n";
     if ( mail($to_email_address,$subject,$message,$header)) {
